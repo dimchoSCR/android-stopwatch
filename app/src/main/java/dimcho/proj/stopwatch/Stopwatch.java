394f663ae1072be251575ca335bc81d2f874360b
dimchoSCR <dimchokarpachev@gmail.com>
2014-11-30 21:00:48 +0200

    Optimized the app by reducing the number of buttons in the layout.(Instead of hiding and showing four buttons I change the background and text of two.)

--- app/src/main/java/dimcho/proj/stopwatch/Stopwatch.java
+++ app/src/main/java/dimcho/proj/stopwatch/Stopwatch.java
@@ -18,14 +18,14 @@ import java.util.TimerTask;
 public class Stopwatch extends Activity {
 
     private Button startButton;
-    private Button resetButton;
+    private Button lapButton;
     private TextView textView;
     private Timer timer;
     private LinearLayout linearLayout;
     private int currentTime = 0;
     private int lapTime = 0;
     private boolean lapViewExists;
-    private boolean isButtonStartPressed;
+    private boolean isButtonStartPressed = false;
 
 
     @Override
@@ -34,7 +34,7 @@ public class Stopwatch extends Activity {
         setContentView(R.layout.activity_stopwatch);
 
         startButton = (Button) findViewById(R.id.btn_start);
-        resetButton = (Button) findViewById(R.id.btn_reset);
+        lapButton = (Button) findViewById(R.id.btn_lap);
         textView = (TextView) findViewById(R.id.stopwatch_view);
         textView.setTextSize(55);
     }
@@ -69,76 +69,88 @@ public class Stopwatch extends Activity {
 */
 
     public void onSWatchStart(View view) {
+        if (isButtonStartPressed) {
+            onSWatchStop();
+        } else {
+            isButtonStartPressed = true;
 
-        startButton.setVisibility(View.GONE);
-        stopButton.setVisibility(View.VISIBLE);
+            startButton.setBackgroundResource(R.drawable.btn_stop_states);
+            startButton.setText(R.string.btn_stop);
 
-        resetButton.setVisibility(View.GONE);
-        lapButton.setVisibility(View.VISIBLE);
+            lapButton.setBackgroundResource(R.drawable.btn_lap_states);
+            lapButton.setText(R.string.btn_lap);
+            lapButton.setEnabled(true);
 
-        timer = new Timer();
-        timer.scheduleAtFixedRate(new TimerTask() {
-            @Override
-            public void run() {
-                runOnUiThread(new Runnable() {
-                    public void run() {
-                        currentTime += 1;
-                        lapTime += 1;
+            timer = new Timer();
+            timer.scheduleAtFixedRate(new TimerTask() {
+                @Override
+                public void run() {
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            currentTime += 1;
+                            lapTime += 1;
 
-                        // update ui
-                        textView.setText(TimeFormatUtil.toDisplayString(currentTime));
+                            // update ui
+                            textView.setText(TimeFormatUtil.toDisplayString(currentTime));
 
 
-                    }
-                });
-            }
-        }, 0, 100);
+                        }
+                    });
+                }
+            }, 0, 100);
+        }
     }
 
-    public void onSWatchStop(View view) {
-        stopButton.setVisibility(View.GONE);
-        startButton.setVisibility(View.VISIBLE);
+    public void onSWatchStop() {
+        startButton.setBackgroundResource(R.drawable.btn_start_states);
+        startButton.setText(R.string.btn_start);
+        lapButton.setEnabled(true);
+        lapButton.setBackgroundResource(R.drawable.btn_lap_states);
+        lapButton.setText(R.string.btn_reset);
 
-        lapButton.setVisibility(View.GONE);
-        resetButton.setEnabled(true);
-        resetButton.setBackgroundResource(R.drawable.btn_lap_states);
-        resetButton.setVisibility(View.VISIBLE);
-
+        isButtonStartPressed = false;
         timer.cancel();
     }
 
-    public void onSWatchReset(View view) {
+    public void onSWatchReset() {
         timer.cancel();
         currentTime = 0;
         lapTime = 0;
         textView.setText(TimeFormatUtil.toDisplayString(currentTime));
-        resetButton.setEnabled(false);
-        resetButton.setBackgroundResource(R.drawable.btn_reset_states);
+        lapButton.setEnabled(false);
+        lapButton.setText(R.string.btn_lap);
+        lapButton.setBackgroundResource(R.drawable.btn_reset_states);
 
         if (lapViewExists) {
             linearLayout.removeAllViews();
-            lapViewExists=false;
+            lapViewExists = false;
         }
     }
+
     public void onSWatchLap(View view) {
+        if(!isButtonStartPressed){
+            onSWatchReset();
+        }else {
 
-        lapViewExists = true;
-        linearLayout = (LinearLayout) findViewById(R.id.layout);
+            lapViewExists = true;
+            linearLayout = (LinearLayout) findViewById(R.id.layout);
 
-        TextView lapDisplay = new TextView(this);
-        ImageView imageView = new ImageView(this);
+            TextView lapDisplay = new TextView(this);
+            ImageView imageView = new ImageView(this);
 
-        lapDisplay.setGravity(Gravity.CENTER);
-        lapDisplay.setTextColor(Color.WHITE);
-        lapDisplay.setTextSize(30);
+            lapDisplay.setGravity(Gravity.CENTER);
+            lapDisplay.setTextColor(Color.WHITE);
+            lapDisplay.setTextSize(30);
 
-        linearLayout.addView(lapDisplay);
-        linearLayout.addView(imageView);
+            linearLayout.addView(lapDisplay);
+            linearLayout.addView(imageView);
 
-        lapDisplay.setText(TimeFormatUtil.toDisplayString(lapTime));
-        imageView.setImageResource(R.drawable.divider);
-        lapTime = 0;
+            lapDisplay.setText(TimeFormatUtil.toDisplayString(lapTime));
+            imageView.setImageResource(R.drawable.divider);
+            lapTime = 0;
+        }
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
