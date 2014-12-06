889b9bfaef51afb8d5a4ef1c5d26dc6fa7182213
dimchoSCR <dimchokarpachev@gmail.com>
2014-12-06 12:02:04 +0200

    Adjusting animation delay

--- app/src/main/java/dimcho/proj/stopwatch/Stopwatch.java
+++ app/src/main/java/dimcho/proj/stopwatch/Stopwatch.java
@@ -136,7 +136,7 @@ public class Stopwatch extends Activity {
             lapViewExists = true;
 
             transition = new LayoutTransition();
-            transition.setStartDelay(LayoutTransition.APPEARING,1);
+            transition.setStartDelay(LayoutTransition.APPEARING,0);
 
             linearLayout = (LinearLayout) findViewById(R.id.layout);
             linearLayout.setLayoutTransition(transition);
