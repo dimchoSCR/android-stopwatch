52b124904baa458aaa1dc99afd0aa675fec6a061
dimchoSCR <dimchokarpachev@gmail.com>
2015-01-27 19:03:34 +0200

    Added Help activity

---
+++ app/src/main/java/dimcho/proj/stopwatch/HelpActivity.java
@@ -0,0 +1,22 @@
+package dimcho.proj.stopwatch;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
+
+
+public class HelpActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+
+        TextView textView = (TextView) findViewById(R.id.help_message);
+
+        textView.setText("Created by Dimcho.\n" +
+                "\nMore information about the app on my gitHub: https://github.com/dimchoSCR/android-stopwatch\n"
+                +"\nOr you can send me an email: dimchokarpachev@gmail.com");
+
+    }
+}
--- app/src/main/java/dimcho/proj/stopwatch/Stopwatch.java
+++ app/src/main/java/dimcho/proj/stopwatch/Stopwatch.java
@@ -2,6 +2,7 @@ package dimcho.proj.stopwatch;
 
 import android.animation.LayoutTransition;
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -172,17 +173,17 @@ public class Stopwatch extends Activity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+    public boolean onOptionsItemSelected (MenuItem item){
+        switch(item.getItemId()){
+            case R.id.action_help:
+                openHelp();
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
     }
 
-
+    public void openHelp(){
+        Intent intent = new Intent(this,HelpActivity.class);
+        startActivity(intent);
+    }
 }
