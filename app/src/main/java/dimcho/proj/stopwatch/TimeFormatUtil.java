f5c46e44babb1a217f2724ce53f28323f3d290c1
dimchoSCR <dimchokarpachev@gmail.com>
2018-06-17 13:07:40 +0300

    Updated TimeFormatUtil to display two millisecond digits

--- app/src/main/java/dimcho/proj/stopwatch/TimeFormatUtil.java
+++ app/src/main/java/dimcho/proj/stopwatch/TimeFormatUtil.java
@@ -4,22 +4,32 @@ public class TimeFormatUtil {
 	
 	public static String toDisplayString(int timeHundreds){
 		int hundreds,seconds,minutes;
-		String formatedSeconds,formatedMinutes;
-		
-		hundreds=timeHundreds%10;
-		seconds=(timeHundreds/=10)%60;
+		// Used to add a '0' at the end of .3 (a one digit number) so it becomes .30
+		// So if the result of timeHundreds%100 equals a one digit number add 0
+		// to the end of the time string
+		// Avoids unnecessary if statements
+		String[] formatterArrayMillis = new String[2];
+		String formattedSeconds,formattedMinutes;
+
+		hundreds=timeHundreds%100;
+		String milliSecStr = Integer.toString(hundreds);
+		formatterArrayMillis[0] = milliSecStr + "0";
+		formatterArrayMillis[1] = milliSecStr;
+
+		seconds=(timeHundreds/=100)%60;
 		minutes=(timeHundreds/=60)%60;
 		
 		//format output
-		formatedSeconds=Integer.toString(seconds/10)+
+		formattedSeconds=Integer.toString(seconds/10)+
 				Integer.toString(seconds%10);
-		formatedMinutes=Integer.toString(minutes/10)+
-				Integer.toString(minutes%10);		
-		
+		formattedMinutes=Integer.toString(minutes/10)+
+				Integer.toString(minutes%10);
+
+		int millSecDigitsCnt = milliSecStr.length();
 		String timeString = 
-				formatedMinutes+":"+
-				formatedSeconds+"."+
-				Integer.toString(hundreds);
+				formattedMinutes+":"+
+				formattedSeconds+"."+
+				formatterArrayMillis[millSecDigitsCnt - 1];
 		
 		
 		return timeString;
